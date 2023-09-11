d77851edfd7ad84719ce37a67f16110d85e42378
Pelasha <144365954+Pelasha@users.noreply.github.com>
2023-09-11 20:33:32 +0300

    Update 15.1.cpp

--- Pelasha/Практика/15.1.cpp
+++ Pelasha/Практика/15.1.cpp
@@ -1,1 +1,37 @@
+include <iostream>
+class Total_price
+{
+    double price;
+    static double max_price;
 
+public:
+    void SetPrice()
+    {
+        std::cout << "Введите цену: ";
+        std::cin >> price;
+    }
+    double CheckPrice()
+    {
+        if (price <= max_price)
+        {
+            return price;
+        }
+        price = max_price;
+        return price;
+    }
+    void show()
+    {
+        std::cout << price;
+    }
+
+};
+double Total_price::max_price = 1000;
+int main()
+{
+    
+    setlocale(LC_ALL, "ru");
+    Total_price P1;
+    P1.SetPrice();
+    P1.CheckPrice();
+    P1.show();
+}
