54cab64f571dd195e5e1bdd2021b5895bb22ca5d
Pelasha <144365954+Pelasha@users.noreply.github.com>
2023-11-06 16:25:08 +0300

    Create Задача_8.cpp

---
+++ Pelasha/Урок 26/Дз/Задача_8.cpp
@@ -0,0 +1,14 @@
+//Задача 8: 
+//Создайте множество, чтобы хранить уникальные имена пользователей.
+//Проверьте, содержит ли множество конкретное имя.
+//
+//Пример работы программы :
+//Входные данные :
+//uniqueNames.insert("Alice");
+//uniqueNames.insert("Bob");
+//uniqueNames.insert("Charlie");
+//
+//std::string nameToCheck = "Alice";
+//
+//Выходные данные :
+//Alice is in the set.
